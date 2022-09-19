16b10e04ea5572859c3bcabd32890265143c23a9
Kunal Sagar <sagar.kunal@yahoo.com>
2022-09-19 19:03:29 +0530

    efficient prime number solution

--- problems/prime.go
+++ problems/prime.go
@@ -5,8 +5,17 @@ func IsPrime(n int) bool {
 	if n == 1 {
 		return false
 	}
-	for i := 2; i*i <= n; i++ {
-		if n%i == 0 {
+
+	if n == 2 || n == 3 {
+		return true
+	}
+
+	if n%2 == 0 || n%3 == 0 {
+		return false
+	}
+
+	for i := 5; i*i <= n; i += 6 {
+		if n%i == 0 || n%(i+2) == 0 {
 			return false
 		}
 	}
