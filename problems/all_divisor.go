11d0e1825f3a4b2986f935c74664d6151cc51e5e
Kunal Sagar <sagar.kunal@yahoo.com>
2022-09-20 21:00:23 +0530

    more optized all divisor problem solved without using golang sort function

--- problems/all_divisor.go
+++ problems/all_divisor.go
@@ -1,21 +1,21 @@
 package problems
 
-import "sort"
-
 // AllDivisorsOfNumber n returns slice of all its divisors
 func AllDivisorsOfNumber(n int) []int {
-	divisors := []int{1}
-
-	for i := 2; i*i <= n; i++ {
+	divisors := []int{}
+	i := 1
+	for ; i*i < n; i++ {
 		if n%i == 0 {
 			divisors = append(divisors, i)
-			if i != (n / i) {
-				divisors = append(divisors, n/i)
-			}
 		}
 	}
 
-	divisors = append(divisors, n)
-	sort.Ints(divisors)
+	for ; i >= 1; i-- {
+		if n%i == 0 {
+			divisors = append(divisors, n/i)
+		}
+	}
+
+	//divisors = append(divisors, n)
 	return divisors
 }
