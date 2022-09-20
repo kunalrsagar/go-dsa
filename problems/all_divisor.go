1db49f8fe0f6b558827a5274e74d355945da55de
Kunal Sagar <sagar.kunal@yahoo.com>
2022-09-20 20:49:37 +0530

    optimized solution for all divisor of number

--- problems/all_divisor.go
+++ problems/all_divisor.go
@@ -1,16 +1,21 @@
 package problems
 
+import "sort"
+
 // AllDivisorsOfNumber n returns slice of all its divisors
 func AllDivisorsOfNumber(n int) []int {
 	divisors := []int{1}
 
-	for i := 2; i <= n/2; i++ {
+	for i := 2; i*i <= n; i++ {
 		if n%i == 0 {
 			divisors = append(divisors, i)
+			if i != (n / i) {
+				divisors = append(divisors, n/i)
+			}
 		}
 	}
 
 	divisors = append(divisors, n)
-
+	sort.Ints(divisors)
 	return divisors
 }
