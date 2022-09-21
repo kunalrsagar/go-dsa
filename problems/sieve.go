73ad600eb18fa9324320e46ddc0c2de193938c24
Kunal Sagar <sagar.kunal@yahoo.com>
2022-09-21 10:56:02 +0530

    more optimized solution

--- problems/sieve.go
+++ problems/sieve.go
@@ -3,24 +3,18 @@ package problems
 // SieveOfEratosthenes function returns slice of all prime number less than input n
 func SieveOfEratosthenes(n int) []int {
 	primes := make([]bool, n+1)
+	primeNumbers := make([]int, 0)
 
 	for i := 2; i <= n; i++ {
 		primes[i] = true
 	}
 
-	for i := 2; i*i <= n; i++ {
+	for i := 2; i <= n; i++ {
 		if IsPrime(i) {
+			primeNumbers = append(primeNumbers, i)
 			for j := i * i; j <= n; j += i {
 				primes[j] = false
 			}
-		}
-	}
-
-	primeNumbers := make([]int, 0)
-
-	for i, v := range primes {
-		if v {
-			primeNumbers = append(primeNumbers, i)
 		}
 	}
 
